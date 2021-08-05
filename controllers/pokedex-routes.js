afe274708d348ea328c3968fe951217ae4c8fafe
Huiran Lin <hugh80082@gmail.com>
2021-08-05 11:49:04 -0700

    Add get routes for pokedex-routes

--- controllers/pokedex-routes.js
+++ controllers/pokedex-routes.js
@@ -1,1 +1,28 @@
 const router = require('express').Router();
+const { Pokedex } = require('../models');
+
+// GET all pokemons for pokedex page
+router.get('/', async (req, res) => {
+  try {
+    const pokemonData = await Pokemon.findAll({});
+
+    res.render('pokedex', {
+      pokemonData,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one pokemon for pokedex page
+router.get('/:id', async (req, res) => {
+  try {
+    const pokemonData = await Pokedex.findByPk(req.params.id);
+
+    res.render('pokedex', { pokemonData });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
