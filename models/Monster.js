0e8d62b026fb389fc0cad40480900a4f19160b0f
Samantha Yuhan <sysweettooth1@gmail.com>
2021-08-05 11:03:54 -0700

    create monster model

---
+++ models/Monster.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Monster extends Model {}
+
+Monster.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    hitpoints: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    isDead: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    moveset: {
+      type: DataTypes.ARRAY,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'monster',
+  }
+);
+
+module.exports = Monster;
