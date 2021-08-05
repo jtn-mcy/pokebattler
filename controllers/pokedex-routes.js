a87637a11fda0c0aa0e614de5d6c5c4ae87946c4
Huiran Lin <hugh80082@gmail.com>
2021-08-05 11:57:52 -0700

    Add Get routes for monsters

--- controllers/pokedex-routes.js
+++ controllers/pokedex-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const { Pokedex } = require('../models');
+const { Pokemon } = require('../models/Pokemon');
+const { Monster } = require('../models/Monster');
 
 // GET all pokemons for pokedex page
 router.get('/', async (req, res) => {
@@ -18,9 +19,33 @@ router.get('/', async (req, res) => {
 // GET one pokemon for pokedex page
 router.get('/:id', async (req, res) => {
   try {
-    const pokemonData = await Pokedex.findByPk(req.params.id);
+    const pokemonData = await Pokemon.findByPk(req.params.id);
 
     res.render('pokedex', { pokemonData });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET all monsters for pokedex page
+router.get('/', async (req, res) => {
+  try {
+    const monsterData = await Monster.findAll({});
+
+    res.render('pokedex', { monsterData });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one monster for pokedex page
+router.get('/:id', async (req, res) => {
+  try {
+    const monsterData = await Monster.findByPk(req.params.id);
+
+    res.render('pokedex', { monsterData });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
