105d5ccb00a50dd2b81dd417862dc8c5ef6694f1
Samantha Yuhan <sysweettooth1@gmail.com>
2021-08-06 10:12:25 -0700

    create level data

---
+++ seeds/levelData.js
@@ -0,0 +1,13 @@
+const { Level } = require('../models');
+
+const leveldata = [
+  {
+    location: "location1",
+    monster_left: 1,
+    game_id: 1,
+  },
+];
+
+const seedGallery = () => Level.bulkCreate(leveldata);
+
+module.exports = seedGallery;
