3756186b35751ac6f29634c0151e3403f3e93c11
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-08-08 00:04:08 -0700

    add howler CDN, navbar beeps

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,25 @@
 //Rendering 
+
+const navBeep = new Howl ({
+    src: ['sounds/nav_item.mp3'],
+    volume: 0.2,
+});
+
+const navBeepConfirm = new Howl ({
+    src: ['sounds/nav_item_confirm.mp3'],
+    volume: 0.2,
+});
+
+const playBeep = () => {
+    console.log('play!')
+    navBeep.play();
+}
+
+// const playConfirm = () => {
+//     console.log('confirm!');
+//     navBeepConfirm.play()
+// }
+
+const navBar_item = document.querySelectorAll('.nav-link');
+navBar_item.forEach((element) => { element.addEventListener('mouseover', playBeep)});
+// navBar_item.forEach((element) => { element.addEventListener('click', playConfirm)});
