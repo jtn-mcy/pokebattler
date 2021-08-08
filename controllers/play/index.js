443c9bbed625a43cad3497cdb8be104d79718199
Huiran Lin <hugh80082@gmail.com>
2021-08-07 19:14:20 -0700

    Add eventlistener to play button in home.handlebars

--- controllers/play/index.js
+++ controllers/play/index.js
@@ -3,8 +3,7 @@ const router = require('express').Router();
 const startRoutes = require('./start-routes');
 const battleRoutes = require('./battle-routes');
 
-
-router.use('/start', startRoutes)
+router.use('/start', startRoutes);
 router.use('/battle', battleRoutes);
 
 module.exports = router;
--- controllers/play/start-routes.js
+++ controllers/play/start-routes.js
@@ -1,47 +1,45 @@
 const router = require('express').Router();
-const { User, Pokemon, Game, Level, Monster } = require('../../models')
+const { User, Pokemon, Game, Level, Monster } = require('../../models');
 
 router.get('/', async (req, res) => {
-    try{
-        const dbGameData = await Game.findOne({
-            where: {
-                user_id: 1,
-                isCurrent: true
-            }
-        });
-        
-        if (!dbGameData) {
-            res.render('game_start', {isCurrent: false})
-        }; //starts a new game
-        const game = dbGameData.get({ plain: true })
-        console.log(game)
-        // res.status(200).json(game);
-        res.render('game_start', { game });
+  try {
+    const dbGameData = await Game.findOne({
+      where: {
+        user_id: 1,
+        isCurrent: true,
+      },
+    });
 
-    } catch (err) {
-        req.status(500).json(err);
-    }
-})
+    if (!dbGameData) {
+      res.render('game_start', { isCurrent: false });
+    } //starts a new game
+    const game = dbGameData.get({ plain: true });
+    console.log(game);
+    // res.status(200).json(game);
+    res.render('game_start', { game });
+  } catch (err) {
+    req.status(500).json(err);
+  }
+});
 
 //CREATE NEW STARTER POKEMON
 router.post('/pokemons', async (req, res) => {
-    try{
-        const newPokemon = await Pokemon.create({
-            name: req.body.name,
-            description: req.body.description,
-            hitpoints: req.body.hitpoints,
-            is_current: true,
-            move_one: req.body.move_one,
-            sprite: req.body.sprite,
-            user_id: req.session.user_id,
-        });
+  try {
+    const newPokemon = await Pokemon.create({
+      name: req.body.name,
+      description: req.body.description,
+      hitpoints: req.body.hitpoints,
+      is_current: true,
+      move_one: req.body.move_one,
+      sprite: req.body.sprite,
+      user_id: req.session.user_id,
+    });
 
-        const pokemon = newPokemon.get({ plain: true });
-        res.status(200).json(pokemon)
-    } catch (err) {
-        res.status(500).json(err);
-    }
-})
-
+    const pokemon = newPokemon.get({ plain: true });
+    res.status(200).json(pokemon);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
